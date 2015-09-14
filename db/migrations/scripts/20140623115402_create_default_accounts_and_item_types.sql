e13f11b25dcf55ec917fb52bbb40622255ea04b6
Pamela Ocampo <pamela.ocampo@gmail.com>
2015-09-14 18:15:00 +0200

    [Pam] - Removing references to freewheelers domains

--- db/migrations/scripts/20140623115402_create_default_accounts_and_item_types.sql
+++ db/migrations/scripts/20140623115402_create_default_accounts_and_item_types.sql
@@ -1,9 +1,9 @@
 -- // create_default_accounts_and_item_types
 -- Migration SQL that makes the change goes here.
 INSERT INTO account (email_address, account_name, password, phone_number, enabled)
-            VALUES ('admin@freewheelers.com', 'AdminCat','admin', '', true);
+	    VALUES ('admin@example.com', 'AdminCat','admin', '', true);
 INSERT INTO account (email_address, account_name, password, phone_number, enabled)
-            VALUES ('user@freewheelers.com', 'UserCat','user', '', true);
+	    VALUES ('user@example.com', 'UserCat','user', '', true);
 
 INSERT INTO account_role (account_name, role) VALUES ('AdminCat', 'ROLE_ADMIN');
 INSERT INTO account_role (account_name, role) VALUES ('UserCat', 'ROLE_USER');
@@ -15,5 +15,5 @@ INSERT INTO item_type (name) VALUES ('Accessories');
 -- //@UNDO
 -- SQL to undo the change goes here.
 
-DELETE FROM account WHERE email_address IN ('admin@freewheelers.com', 'user@freewheelers.com');
+DELETE FROM account WHERE email_address IN ('admin@example.com', 'user@example.com');
 DELETE FROM item_type WHERE name IN ('Frames', 'Accessories');

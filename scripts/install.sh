6517c8ea2621d9c3b74dbd4f84ab2e4ec94bbcb8
tklae <github@klaesener.net>
2016-03-04 17:45:23 +0530

    Rebecca/Tim #0 Hopefully better error handling for install script, second try

--- scripts/install.sh
+++ scripts/install.sh
@@ -12,55 +12,47 @@ if [ ! -e "dist/freewheelers.zip" ]; then
   exit -1
 fi;
 
-function checkJava {
-    type -p java > /dev/null || (echo "java not found" && exit -1)
-}
-
-function uploadFreewheelers {
-    scp dist/freewheelers.zip ${USER}@${HOST}:/tmp
-}
-
-function connectAndInstall {
-    ssh ${USER}@${HOST} /bin/bash << EOF
-
-    sudo su appuser
-
-    cd /tmp/
-    jetty_path=/tmp/jetty-runner-8.1.14.v20131031.jar
-    if ! [ -f \$jetty_path ];then
-      curl -O http://central.maven.org/maven2/org/mortbay/jetty/jetty-runner/8.1.14.v20131031/jetty-runner-8.1.14.v20131031.jar
-    fi
-
-    #Create directory and move app
-    sudo chown appuser:user /tmp/freewheelers.zip
-    TIMESTAMP=\$(date +"%Y-%m-%d-%HH%MM%Ss")
-    mkdir -p /home/appuser/freewheelers/\$TIMESTAMP || exit 1
-    mv /tmp/freewheelers.zip /home/appuser/freewheelers/\$TIMESTAMP || exit 1
-    cd /home/appuser/freewheelers/\$TIMESTAMP
-    unzip freewheelers.zip
-    cp \$jetty_path /home/appuser/freewheelers/\$TIMESTAMP
-    ln -nfs /home/appuser/freewheelers/\$TIMESTAMP /home/appuser/freewheelers/current_version
-
-    #Stop app service
-    sudo sh /etc/init.d/freewheelers stop
-
-    #DB migrations
-    sh db/migrations/mybatis/bin/migrate --path=./db/migrations up
-
-    #Start app service
-    sudo cp scripts/freewheelers.init /etc/init.d/freewheelers
-    sudo chmod 0755 /etc/init.d/freewheelers
-    sudo chown root:root /etc/init.d/freewheelers
-    sudo sh /etc/init.d/freewheelers start
-    EOF
-}
-
+#Setup error handling
 function errorHandler {
     echo "An error occured while installing the Freewheelers application"
     exit 1
 }
 trap errorHandler SIGINT SIGTERM ERR
 
-checkJava
-uploadFreewheelers
-connectAndInstall
+#Start actual installation
+type -p java > /dev/null || (echo "java not found" && exit -1)
+
+scp dist/freewheelers.zip ${USER}@${HOST}:/tmp
+
+ssh ${USER}@${HOST} /bin/bash << EOF
+
+sudo su appuser
+
+cd /tmp/
+jetty_path=/tmp/jetty-runner-8.1.14.v20131031.jar
+if ! [ -f \$jetty_path ];then
+  curl -O http://central.maven.org/maven2/org/mortbay/jetty/jetty-runner/8.1.14.v20131031/jetty-runner-8.1.14.v20131031.jar
+fi
+
+#Create directory and move app
+sudo chown appuser:user /tmp/freewheelers.zip
+TIMESTAMP=\$(date +"%Y-%m-%d-%HH%MM%Ss")
+mkdir -p /home/appuser/freewheelers/\$TIMESTAMP || exit 1
+mv /tmp/freewheelers.zip /home/appuser/freewheelers/\$TIMESTAMP || exit 1
+cd /home/appuser/freewheelers/\$TIMESTAMP
+unzip freewheelers.zip
+cp \$jetty_path /home/appuser/freewheelers/\$TIMESTAMP
+ln -nfs /home/appuser/freewheelers/\$TIMESTAMP /home/appuser/freewheelers/current_version
+
+#Stop app service
+sudo sh /etc/init.d/freewheelers stop
+
+#DB migrations
+sh db/migrations/mybatis/bin/migrate --path=./db/migrations up
+
+#Start app service
+sudo cp scripts/freewheelers.init /etc/init.d/freewheelers
+sudo chmod 0755 /etc/init.d/freewheelers
+sudo chown root:root /etc/init.d/freewheelers
+sudo sh /etc/init.d/freewheelers start
+EOF

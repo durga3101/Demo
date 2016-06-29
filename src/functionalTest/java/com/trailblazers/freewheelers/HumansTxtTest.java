b4ef986634260a3eed6cce4df4ccc62dae9ca699
ellaholmes <eholmes@thoughtworks.com>
2016-06-29 16:36:23 +0530

    added Ella Holmes to functional test

--- src/functionalTest/java/com/trailblazers/freewheelers/HumansTxtTest.java
+++ src/functionalTest/java/com/trailblazers/freewheelers/HumansTxtTest.java
@@ -36,7 +36,6 @@ public class HumansTxtTest {
         String[] split = element.getText().split("\n");
         assertThat(split, is(expectedTeamMembers));
         assertThat(split.length, is(3));
-
     }
 
 }

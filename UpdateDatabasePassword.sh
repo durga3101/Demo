97ab5a22b2fa1621435afb9c9666da3a8cc93d45
Julio <jcomfilho@gmail.com>
2016-07-09 15:00:02 +0530

    [Julio/Prajakta][#180-#166] Added task to encrypt existing passwords

---
+++ UpdateDatabasePassword.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd  src/main/java/com/trailblazers/freewheelers
+javac  UpdateDatabasePassword.java
+java UpdateDatabasePassword
+echo hi there..........................
---
+++ src/main/java/com/trailblazers/freewheelers/UpdateDatabasePassword.java
@@ -0,0 +1,19 @@
+import com.trailblazers.freewheelers.mappers.AccountMapper;
+import com.trailblazers.freewheelers.mappers.MyBatisUtil;
+import com.trailblazers.freewheelers.model.Account;
+import org.apache.ibatis.session.SqlSession;
+
+import java.util.List;
+
+/**
+ * Created by jmariano on 7/9/16.
+ */
+public class UpdateDatabasePassword {
+    public static void main(String[] args)
+    {
+//        SqlSession sqlSession = MyBatisUtil.getSqlSessionFactory().openSession();
+//        AccountMapper mapper = sqlSession.getMapper(AccountMapper.class);
+//        List<Account> accountList = mapper.getAllAccounts();
+        //System.out.println("Dummy class ************"+accountList.size());
+    }
+}

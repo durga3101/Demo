7713aaa037c64b6229516b81a155e469bed3d306
Rufus Raghunath <rraghun@thoughtworks.com>
2016-07-11 18:42:45 +0530

    [Rufus/Durga][#149] Create Gateway Controller

---
+++ src/main/java/com/trailblazers/freewheelers/web/GatewayController.java
@@ -0,0 +1,55 @@
+package com.trailblazers.freewheelers.web;
+
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+
+
+@Controller
+@RequestMapping("/gateway")
+public class GatewayController {
+
+    private
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public String post(@RequestParam(value = "card_number", required = true) String cc_number,
+                       @RequestParam(value = "card_ccv", required = true) String csc,
+                       @RequestParam(value = "expiry_month", required = true) String expiry_month,
+                       @RequestParam(value = "expiry_year", required = true) String expiry_year,
+                       @RequestParam(value = "amount", required = true) String amount
+    ) {
+
+        String expiry = expiry_month + "-" + expiry_year;
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_XML);
+
+        String body = "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
+                            "<authorisation-request>" +
+                                "<cc_number>" + cc_number + "</cc_number>" +
+                                    "<csc>" + csc + "</csc>" +
+                                    "<expiry>" + expiry + "</expiry>" +
+                                "<amount>" + amount + "</amount>" +
+                            "</authorisation-request>";
+
+        HttpEntity<String> request = new HttpEntity<>(body, headers);
+        ResponseEntity<String> response = restTemplate.postForEntity("http://ops.freewheelers.bike:5000/authorise", request, String.class);
+
+
+
+
+
+
+
+        // TODO: redirect to success/failure view
+        return "redirect:/";
+    }
+}
---
+++ src/test/java/com/trailblazers/freewheelers/web/GatewayControllerTest.java
@@ -0,0 +1,7 @@
+package com.trailblazers.freewheelers.web;
+
+/**
+ * Created by rraghun on 11/07/2016.
+ */
+public class GatewayControllerTest {
+}

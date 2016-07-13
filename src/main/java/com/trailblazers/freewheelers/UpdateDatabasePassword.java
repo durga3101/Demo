fe882d6fc24181dc0c62262ecacbd70d9e6955dc
lukebelliveau <lbellive@thoughtworks.com>
2016-07-13 14:05:21 +0530

    [Julio/Luke]#184 uncommented code to encrypt data in production

--- src/main/java/com/trailblazers/freewheelers/UpdateDatabasePassword.java
+++ src/main/java/com/trailblazers/freewheelers/UpdateDatabasePassword.java
@@ -1,40 +1,40 @@
-//package com.trailblazers.freewheelers;
-//
-//import com.trailblazers.freewheelers.mappers.AccountMapper;
-//import com.trailblazers.freewheelers.model.Account;
-//import org.apache.ibatis.session.SqlSession;
-//
-//import java.util.List;
-//
-//
-//public class UpdateDatabasePassword {
-//    private AccountMapper mapper;
-//    private SqlSession sqlSession;
-//
-//    public UpdateDatabasePassword(AccountMapper mapper, SqlSession sqlSession) {
-//        this.mapper = mapper;
-//        this.sqlSession = sqlSession;
-//    }
-//
-//    public void updateDatabaseToEncryptPassword()
-//    {
-//
-//        List<Account> accountList = mapper.getAllAccounts();
-//        for (Account account: accountList ) {
-//            String oldPassword = account.getPassword();
-//            System.out.println("name:"+account.getAccount_name()+"Country::"+account.getCountry()+ "ld password:"+oldPassword);
-//
-//            if(account.getCountry() == null)
-//            {
-//                mapper.updatePassword(account);
-//            }
-//            System.out.println("New password:"+account.getPassword());
-//
-//            sqlSession.commit();
-//
-//        }
-//
-//        sqlSession.close();
-//
-//    }
-//}
+package com.trailblazers.freewheelers;
+
+import com.trailblazers.freewheelers.mappers.AccountMapper;
+import com.trailblazers.freewheelers.model.Account;
+import org.apache.ibatis.session.SqlSession;
+
+import java.util.List;
+
+
+public class UpdateDatabasePassword {
+    private AccountMapper mapper;
+    private SqlSession sqlSession;
+
+    public UpdateDatabasePassword(AccountMapper mapper, SqlSession sqlSession) {
+        this.mapper = mapper;
+        this.sqlSession = sqlSession;
+    }
+
+    public void updateDatabaseToEncryptPassword()
+    {
+
+        List<Account> accountList = mapper.getAllAccounts();
+        for (Account account: accountList ) {
+            String oldPassword = account.getPassword();
+            System.out.println("name:"+account.getAccount_name()+"Country::"+account.getCountry()+ "ld password:"+oldPassword);
+
+            if(account.getCountry() == null)
+            {
+                mapper.updatePassword(account);
+            }
+            System.out.println("New password:"+account.getPassword());
+
+            sqlSession.commit();
+
+        }
+
+        sqlSession.close();
+
+    }
+}

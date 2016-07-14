c66ceb2caa74b4c04e744fa8920c9e3fb3f2f4ab
Rufus Raghunath <rraghun@thoughtworks.com>
2016-07-14 09:11:00 +0530

    [Rufus] Resolve merge conflicts with master

--- src/test/java/com/trailblazers/freewheelers/web/PaymentControllerTest.java
+++ src/test/java/com/trailblazers/freewheelers/web/PaymentControllerTest.java
@@ -1,43 +1,63 @@
 package com.trailblazers.freewheelers.web;
 
 import com.trailblazers.freewheelers.model.Item;
+import com.trailblazers.freewheelers.service.ItemService;
+import com.trailblazers.freewheelers.service.impl.ItemServiceImpl;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.ui.Model;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.math.BigDecimal;
-import java.security.Principal;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class PaymentControllerTest {
     private HttpSession httpSession;
     private HttpServletRequest request;
-    @Test
-    public void shouldPutCorrectTotalAsAttribute() {
-        Model model = mock(Model.class);
-        Principal principal = mock(Principal.class);
-        Item item = new Item();
-        item.setPrice(BigDecimal.valueOf(10));
+    private Model model;
+    private PaymentController controller;
+    private ItemService service;
+    private String expected;
 
-        PaymentController paymentController = new PaymentController();
 
-        paymentController.payForItem(model, principal, item);
-
-        verify(model).addAttribute("totalAmount", "10");
-    }
-    @Test
-    public void addToCartShouldReturnLoginWhenPrincipalIsNull() throws Exception {
+    @Before
+    public void setUp() throws Exception {
+        service = mock(ItemServiceImpl.class);
+        controller = new PaymentController(service);
         request = mock(HttpServletRequest.class);
-        httpSession =mock(HttpSession.class);
+        httpSession = mock(HttpSession.class);
+        model = mock(Model.class);
         when(request.getSession()).thenReturn(httpSession);
-        Model model = mock(Model.class);
-        assertEquals("payment", new PaymentController().get(model,request));
+        expected = "payment";
 
     }
+
+    @Test
+    public void shouldRedirectToPaymentBeforeCreatingItemModelWhenNoItemInCart() throws Exception {
+        when(httpSession.getAttribute(anyString())).thenReturn(null);
+
+        String actual = controller.get(model,request);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldRedirectToPaymentAfterCreatingItemModelWhenItemInCart() throws Exception {
+        Item item = mock(Item.class);
+        when(httpSession.getAttribute(anyString())).thenReturn(item);
+        when(service.get(anyLong())).thenReturn(item);
+
+        String actual = controller.get(model,request);
+
+        assertEquals(expected, actual);
+
+    }
+
 
 }

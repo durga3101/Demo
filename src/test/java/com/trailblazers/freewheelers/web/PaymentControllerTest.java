06a0f0c788a5c038d88c2ef7494a92f9cf985aef
lukebelliveau <lbellive@thoughtworks.com>
2016-07-18 21:14:03 +0530

    Revert "[Luke]#189 calculating total price to use in Payment page"
    This reverts commit b818e3c7159fe1030b7f4520fd34bc9737d1a1c9.

--- src/test/java/com/trailblazers/freewheelers/web/PaymentControllerTest.java
+++ src/test/java/com/trailblazers/freewheelers/web/PaymentControllerTest.java
@@ -10,13 +10,10 @@ import org.springframework.ui.Model;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import java.math.BigDecimal;
-import java.util.HashMap;
-
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Matchers.*;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class PaymentControllerTest {
@@ -62,41 +59,5 @@ public class PaymentControllerTest {
 
     }
 
-    //Will be moved to calculator test class when merge conflicts resolved
-    @Test
-    public void shouldComputeCorrectTotalPriceFromShoppingCart() {
-        Item item1 = mock(Item.class);
-        Item item2 = mock(Item.class);
-        when(item1.getPrice()).thenReturn(new BigDecimal(10.00));
-        when(item2.getPrice()).thenReturn(new BigDecimal(20.00));
-        HashMap<Item, Long> cartMap = new HashMap<>();
-        cartMap.put(item1, new Long(2));
-        cartMap.put(item2, new Long(3));
-
-        when(service.getItemHashMap(any(HashMap.class))).thenReturn(cartMap);
-
-        assertEquals(new BigDecimal("80.00"), controller.getTotalPriceFromCart(request));
-
-    }
-
-    @Test
-    public void shouldAddTotalPriceToModel() {
-        Item item = mock(Item.class);
-        when(httpSession.getAttribute(anyString())).thenReturn(item);
-        when(service.get(anyLong())).thenReturn(item);
-
-        Item item1 = mock(Item.class);
-        Item item2 = mock(Item.class);
-        when(item1.getPrice()).thenReturn(new BigDecimal(10.00));
-        when(item2.getPrice()).thenReturn(new BigDecimal(20.00));
-        HashMap<Item, Long> cartMap = new HashMap<>();
-        cartMap.put(item1, new Long(2));
-        cartMap.put(item2, new Long(3));
-
-        when(service.getItemHashMap(any(HashMap.class))).thenReturn(cartMap);
-
-        controller.get(model, request);
-//        verify(model).addAttribute("totalAmount", totalPrice);
-    }
 
 }

fffb2cdd756c59fb2abf36ec4c3ea618fbccfb89
Vankayalapati Nagadurga <nvankaya@thoughtworks.com>
2016-07-20 16:37:45 +0530

    [Durga/Raju][#198]Add order table to database

---
+++ db/migrations/scripts/20160720105433_create_order.sql
@@ -0,0 +1,17 @@
+-- // create_order
+-- Migration SQL that makes the change goes here.
+
+CREATE TABLE order
+(
+  order_id SERIAL PRIMARY KEY,
+  account_id BIGINT NOT NULL,
+  reserve_order_id BIGINT NOT NULL ,
+  status CHARACTER VARYING(255) NOT NULL,
+  reservation_timestamp TIMESTAMP without time zone NOT NULL
+);
+
+-- //@UNDO
+-- SQL to undo the change goes here.
+DROP TABLE order
+
+

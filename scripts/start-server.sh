d00f4ef544cc5e96f54df296a3e064fb4c64c705
Rufus Raghunath <rraghun@thoughtworks.com>
2016-07-21 00:07:22 +0530

    [Rufus][QA] Add live setting to start-server.sh

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 PATH_TO_FREEWHEELERS=/home/appuser/freewheelers/current_version
-printf "\n" | JETTY_BASE=$PATH_TO_FREEWHEELERS java -XX:MaxPermSize=256M -jar $PATH_TO_FREEWHEELERS/jetty-runner-8.1.14.v20131031.jar \
+printf "\n" | JETTY_BASE=$PATH_TO_FREEWHEELERS java -XX:MaxPermSize=256M -jar $PATH_TO_FREEWHEELERS/jetty-runner-8.1.14.v20131031.jar -Dspring .profiles .active=live \
 		--port 8080 --log $PATH_TO_FREEWHEELERS/freewheelers.log \
 		$PATH_TO_FREEWHEELERS/freewheelers.war > $PATH_TO_FREEWHEELERS/server.log &

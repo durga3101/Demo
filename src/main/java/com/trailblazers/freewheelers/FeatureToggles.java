2544c11f7dabac36686d2a36a1905cecfa7e6737
Prajakta Desai <pdesai@thoughtworks.com>
2016-07-21 17:15:35 +0530

    [prajakta/julio][#200] Feature toggle ON for dislay address on user profile page

--- src/main/java/com/trailblazers/freewheelers/FeatureToggles.java
+++ src/main/java/com/trailblazers/freewheelers/FeatureToggles.java
@@ -2,5 +2,5 @@ package com.trailblazers.freewheelers;
 
 public class FeatureToggles {
 
-    public static final boolean DISPLAY_ADDRESS_ON_USER_PROFILE = false;
+    public static final boolean DISPLAY_ADDRESS_ON_USER_PROFILE = true;
 }

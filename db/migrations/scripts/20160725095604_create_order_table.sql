225999208d4875409082d7a27690dadb873e84c5
Julio <jcomfilho@gmail.com>
2016-07-25 15:44:16 +0530

    [Julio/Siqi][#153] added create_order_table migration

--- db/migrations/scripts/20160725095604_create_order_table.sql
+++ db/migrations/scripts/20160725095604_create_order_table.sql
@@ -1,17 +1,13 @@
--- // create_order
+-- // drop_order
 -- Migration SQL that makes the change goes here.
-
-CREATE TABLE order_table
-(
+CREATE TABLE order_table(
   order_id SERIAL PRIMARY KEY,
   account_id BIGINT NOT NULL,
-  reserve_order_id BIGINT NOT NULL,
   status CHARACTER VARYING(255) NOT NULL,
   reservation_timestamp TIMESTAMP without time zone NOT NULL
 );
 
 -- //@UNDO
 -- SQL to undo the change goes here.
-DROP TABLE order_table
-
+DROP TABLE order_table;
 
